4406435d3d3299884d3a8092ae7d7cd6304b005a
rashokanand <ashok.anand91@gmail.com>
2026-02-10 06:47:59 +0530

    Complete repeatString function

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) return 'ERROR';
+    let nstr = ''
+    for (let i = 0; i < num; i++) {
+        nstr += str;
+    }
+    return nstr;
 };
 
 // Do not edit below this line
