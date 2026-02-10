96537a2874add7000e8bdd9dc94336c2421d9cf1
rashokanand <ashok.anand91@gmail.com>
2026-02-10 07:39:15 +0530

    Complete sumAll function

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,14 @@
-const sumAll = function() {
+const sumAll = function(from, to) {
+
+    if (from < 0 || to < 0 || !Number.isInteger(from + to)) return 'ERROR';
+
+    let sum = (from > to)? to: from; //Initialize sum to the smaller number
+    let larger = sum == from? to: from;
+
+    for (let i = sum + 1; i <= larger; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
