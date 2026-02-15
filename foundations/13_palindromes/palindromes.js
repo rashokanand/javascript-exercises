43562a232f752a72618fb037738d271acfa0f752
rashokanand <ashok.anand91@gmail.com>
2026-02-15 08:33:59 +0530

    Palindromes complete

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const regex = /\W/g;
+    // const regex2 = /\p{P}/u;
+    let str = string.replace(regex,"").toLowerCase();
+    return str.split("").reverse().join("") === str;
 };
 
 // Do not edit below this line
-module.exports = palindromes;
+module.exports = palindromes;3
