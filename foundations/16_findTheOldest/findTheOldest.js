87c5879706e627305001737d012a563e1e176ab1
rashokanand <ashok.anand91@gmail.com>
2026-02-15 15:21:16 +0530

    Complete oldestperson

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const date = new Date();
+    const year = date.getFullYear();
 
+    return arr.sort((a,b) => {
+        let aAge = "yearOfDeath" in a? a.yearOfDeath - a.yearOfBirth: year - a.yearOfBirth;
+        let bAge = "yearOfDeath" in b? b.yearOfDeath - b.yearOfBirth: year - b.yearOfBirth;
+        return aAge - bAge;
+    })[arr.length - 1]
 };
 
 // Do not edit below this line
